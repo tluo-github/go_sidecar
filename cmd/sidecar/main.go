9ec3bb1eba1c59163c9fea39b2b034e6e513e7b1
luotao <tluolovebruce@gmail.com>
2018-04-16 10:23:40 +0800

    add proxy

---
+++ cmd/sidecar/main.go
@@ -0,0 +1,6 @@
+package sidecar
+
+import "github.com/tluolovembtan/go_sidecar/http_proxy"
+func main() {
+	http_proxy.Show()
+}
---
+++ http_proxy/proxy.go
@@ -0,0 +1,7 @@
+package http_proxy
+
+import "fmt"
+
+func Show()  {
+	fmt.Println("this is proxy")
+}
