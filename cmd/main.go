fcec34147767b763ff741c76511acd53e7779666
luotao <tluolovebruce@gmail.com>
2018-04-16 10:26:39 +0800

    update

---
+++ cmd/main.go
@@ -0,0 +1,6 @@
+package cmd
+
+import "../http_proxy"
+func main() {
+	http_proxy.Show()
+}
--- cmd/sidecar/main.go
+++
@@ -1,6 +0,0 @@
-package sidecar
-
-import "github.com/tluolovembtan/go_sidecar/http_proxy"
-func main() {
-	http_proxy.Show()
-}
